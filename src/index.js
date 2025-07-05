3b8bb28d336063702f8231774763ed68f3a682fa
Eman <emanabukhater2001@gmail.com>
2025-07-05 14:50:30 +0300

    Generate the client again

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express"); // Express framework for building web applications
+const cors = require("cors");// Middleware for enabling CORS (Cross-Origin Resource Sharing) and allowing requests from different origins
+const dotenv = require("dotenv"); // Module for loading environment variables from a .env file
+const { PrismaClient } = require("@prisma/client");
+
+dotenv.config();
+
+const app = express(); // Initialize the Express application
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Article API is running!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
